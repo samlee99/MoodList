d9461a8f645bc402a98d26a6073c0c96d897522f
samlee99 <sam1994lee@gmail.com>
2017-04-18 21:54:34 -0700

    Added an activity for the photo taking screen and Microsoft's Face and Emotion API into the app build.gradle file.

---
+++ app/src/main/java/com/example/samlee/moodlist/PhotoActivity.java
@@ -0,0 +1,18 @@
+package com.example.samlee.moodlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by samlee on 4/12/17.
+ */
+
+public class PhotoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
