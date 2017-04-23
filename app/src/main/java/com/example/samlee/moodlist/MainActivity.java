e00ee9146dd12e051fbadf6f81c15919221eb1ed
samlee99 <sam1994lee@gmail.com>
2017-04-23 11:51:09 -0700

    Added buttons for camera and gallery.  Login button just goes straight to activity, still need to implement logins.  Added subscription keys to strings.xml file, but still need to implement emotion recognition.

--- app/src/main/java/com/example/samlee/moodlist/MainActivity.java
+++ app/src/main/java/com/example/samlee/moodlist/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.samlee.moodlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.loginButton)
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.loginButton)
+    void loginButtonClick() {
+        Intent intent = new Intent(this, PhotoActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/samlee/moodlist/PhotoActivity.java
+++ app/src/main/java/com/example/samlee/moodlist/PhotoActivity.java
@@ -3,12 +3,20 @@ package com.example.samlee.moodlist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 /**
  * Created by samlee on 4/12/17.
  */
 
 public class PhotoActivity extends Activity {
+
+    @BindView(R.id.cameraButton)
+    Button cameraButton;
+    @BindView(R.id.galleryButton)
+    Button galleryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
